c1b2ec9d48799949ed6127832f3920e900223e76
Garnero <elliot.garnero@epitech.eu>
2020-08-05 16:17:20 +0200

    test des commits

---
+++ assets/js/components/delivery/MainDeliveryModal.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+class MainDeliveryModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      //   <div>
+      //     <a
+      //       className="text-decoration-none"
+      //       data-toggle="modal"
+      //       data-target="#modal2"
+      //     >
+      //       <i className="far fa-question-circle"></i>
+      //     </a>
+      //     <div className="modal fade modal2" id="modal2">
+      //       <div className="modal-dialog modal-dialog-centered">
+      //         <div className="modal-content">
+      //           <div className="modal-header">
+      //             <h4 className="modal-title">Frais de livraison</h4>
+      //             <button type="button" className="close" data-dismiss="modal">
+      //               &times;
+      //             </button>
+      //           </div>
+      //           <div className="modal-body">
+      //             <h6>Ah ok lol mdr vas y</h6>
+      //             <div className="container"></div>
+      //           </div>
+      //         </div>
+      //       </div>
+      //     </div>
+      //   </div>
+      <div>
+        <p>ok</p>
+      </div>
+    );
+  }
+}
+
+export default MainDeliveryModal;
